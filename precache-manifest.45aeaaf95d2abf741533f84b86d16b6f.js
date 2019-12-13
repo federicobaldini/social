524ce1bb9b46f492a509778c1c12b397abcb765e
federico baldini <58589983+baldoIF@users.noreply.github.com>
2019-12-13 01:05:16 +0100

    Updates

---
+++ precache-manifest.45aeaaf95d2abf741533f84b86d16b6f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514ad3e9c1297d0de5adf3af358813b8",
+    "url": "/social-page/index.html"
+  },
+  {
+    "revision": "0d2f42bbf00ca59a874d",
+    "url": "/social-page/static/css/main.5c4fdc90.chunk.css"
+  },
+  {
+    "revision": "2b88bbbcf1a2efe8abbf",
+    "url": "/social-page/static/js/2.29901ab1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/social-page/static/js/2.29901ab1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d2f42bbf00ca59a874d",
+    "url": "/social-page/static/js/main.09f7a346.chunk.js"
+  },
+  {
+    "revision": "dc31af709a38900995d1",
+    "url": "/social-page/static/js/runtime-main.56a9826b.js"
+  },
+  {
+    "revision": "0682e129a9cb7f8c4b0774d9354de29b",
+    "url": "/social-page/static/media/food.0682e129.jpg"
+  },
+  {
+    "revision": "8fcaa3c19a30687cce4ce0c1dcebc759",
+    "url": "/social-page/static/media/forest.8fcaa3c1.jpg"
+  },
+  {
+    "revision": "6c0e84d430141b514393924e6b9d64ac",
+    "url": "/social-page/static/media/jojo.6c0e84d4.jpg"
+  },
+  {
+    "revision": "ba4cba482e32a4c8147b5d0b8f8c827b",
+    "url": "/social-page/static/media/like.ba4cba48.png"
+  },
+  {
+    "revision": "73d1f3e12441ac6f0b4573d0ae421327",
+    "url": "/social-page/static/media/loading.73d1f3e1.gif"
+  },
+  {
+    "revision": "65ffa5ba8be30c2385827338d50a227b",
+    "url": "/social-page/static/media/sprites.65ffa5ba.png"
+  },
+  {
+    "revision": "a9e88729a2c45d20e62591064bc5e11b",
+    "url": "/social-page/static/media/user.a9e88729.png"
+  },
+  {
+    "revision": "44841d9fe08421da903b2d2769be0b26",
+    "url": "/social-page/static/media/user2-1.44841d9f.png"
+  },
+  {
+    "revision": "3556d69002ae1dfc082edf97b6d4d81f",
+    "url": "/social-page/static/media/user2-2.3556d690.png"
+  },
+  {
+    "revision": "7b417275fcca44df7fee78849920b291",
+    "url": "/social-page/static/media/user2.7b417275.png"
+  },
+  {
+    "revision": "14d23b9279dbf5fa5bc88794d2cd1c47",
+    "url": "/social-page/static/media/wine.14d23b92.jpg"
+  }
+]);
